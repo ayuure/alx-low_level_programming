8003a3e58d206ec2f290464948d28da681187ef4
ayuure <bertinaayuure0147@gmail.com>
2023-09-07 21:22:10 +0000

    message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  * Return: Always 0 (Success)
  */
 int main(void){
-printf("size of a char: %zu byte(s)",sizeof(char));
-printf("size of an int: %zu byte(s)",sizeof(int));
-printf("size of a long int: %zu byte(s)",sizeof(long int));
-printf("size of a long long int: %zu byte(s)",sizeof(long long int));
-printf("size of a float: %zu byte(s)",sizeof(float));
+printf("size of a char: %c byte(s)",sizeof(char));
+printf("size of an int: %d byte(s)",sizeof(int));
+printf("size of a long int: %ld byte(s)",sizeof(long int));
+printf("size of a long long int: %lld byte(s)",sizeof(long long int));
+printf("size of a float: %lf byte(s)",sizeof(float));
 return (0);
 }
