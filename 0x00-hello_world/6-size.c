f88a05099613121c6a79c636865cfa04ea5c98eb
ayuure <bertinaayuure0147@gmail.com>
2023-09-07 20:30:57 +0000

    added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,5 @@ printf("size of an int: %zu byte(s)",sizeof(int));
 printf("size of a long int: %zu byte(s)",sizeof(long int));
 printf("size of a long long int: %zu byte(s)",sizeof(long long int));
 printf("size of a float: %zu byte(s)",sizeof(float));
-return 0;
+return (0);
 }
