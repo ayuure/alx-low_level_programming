8e66a08ab7bcce8e6e1b1c06bc2bb9f05f383edf
ayuure <bertinaayuure0147@gmail.com>
2023-09-07 20:08:35 +0000

    files added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - a program that uses puts
+*
+* Return: 0 (Sucess)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return 0;
+
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - a program that prints sizes of types
+*
+* Return: 0 (Sucess)
+*/
+int main(void){
+printf("size of a char: %zu byte(s)",sizeof(char));
+printf("size of an int: %zu byte(s)",sizeof(int));
+printf("size of a long int: %zu byte(s)",sizeof(long int));
+printf("size of a long long int: %zu byte(s)",sizeof(long long int));
+printf("size of a float: %zu byte(s)",sizeof(float));
+return 0;
+}
