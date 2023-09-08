28df666e1a27351282042cac971c82d05f15342e
ayuure <bertinaayuure0147@gmail.com>
2023-09-08 15:13:35 +0000

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+	int a;
+	int p;
+
+	for (a = 48; a <= 56; a++)
+	{
+		for (p = 49; p <= 57; p++)
+		{
+			if (p > a)
+			{
+				putchar(a);
+				putchar(p);
+				if (a != 56 || p != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+	}
