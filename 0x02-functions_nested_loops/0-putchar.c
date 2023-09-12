2200515872568f802fa7d1f354d9ad294bace400
ayuure <bertinaayuure0147@gmail.com>
2023-09-12 11:56:18 +0000

    first file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,13 @@
 int main(void)
 	{
 	_putchar('c');
+	_putchar('-');
+	_putchar('h');
+	_putchar('p');
+	_putchar('y');
+	_putchar('8');
+	_putchar('\n');
+	_putchar('6');
 	_putchar('\n');
 	return (0);
 	}
