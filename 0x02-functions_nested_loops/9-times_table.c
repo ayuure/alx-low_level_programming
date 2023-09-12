f1f4549781dc2677624d887a61cc150e0aa98f96
ayuure <bertinaayuure0147@gmail.com>
2023-09-12 21:59:43 +0000

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,35 +6,33 @@
  */
 void times_table(void)
 {
-	int i;
-	int j;
-	int sum = 0;
+	int i, j, k;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			sum = i * j;
-		}
+			k = j * i;
 			if (j == 0)
 			{
-				_putchar(sum + '0');
+				_putchar(k + '0');
 			}
 
-			else if (sum < 10 && j != 0)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(sum + '0');
-			} else if (sum >= 10)
+				_putchar(k + '0');
+			} else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((sum / 10) + '0');
-				_putchar((sum % 10) + '0');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
-	return;
+	}
 }
+
