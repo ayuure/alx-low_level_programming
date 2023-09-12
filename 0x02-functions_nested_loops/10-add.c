bd58636d060268a0f3e2ad3d173352521504a97e
ayuure <bertinaayuure0147@gmail.com>
2023-09-12 21:07:13 +0000

    a +b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add- addition of values
+ *@a:value one
+ *@b:value two
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
