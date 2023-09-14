a52334d029bedf45bf8891c91d94a62bc0fdef4d
ayuure <bertinaayuure0147@gmail.com>
2023-09-14 15:16:30 +0000

    test

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_isupper - checks if a charcter is upper case
+ *@c: value to be checker
+ *
+ *Return 1: uppercase
+ */
+int _isupper(int c)
+	{
+		if (isupper(c))
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
