914de6c0a9f5973c232303d5e377d4814c4f572f
ayuure <bertinaayuure0147@gmail.com>
2023-09-21 20:08:29 +0000

    two

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - concatenate
+ *@dest: one
+ *@src: two
+ *@n : number
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] = '\0')
+	{
+		i++;
+	}
+	while (j <= n && src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
