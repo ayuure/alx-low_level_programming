56b8cb291c2e6718af48b9b604b70f12b73ff462
ayuure <bertinaayuure0147@gmail.com>
2023-10-06 16:01:05 +0000

    fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,71 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
-/**
- * convert_to - check - string there are digit
- * @str: array str
- *
- * Return: Always 0 (Success)
- */
-int convert_to(char *str)
-{
-
-	unsigned int c;
-
-	c = 0;
-	while (c < strlen(str))
-
-	{
-		if (!isdigit(str[c])) /*check if str there are digit*/
-		{
-			return (0);
-		}
-
-		c++;
-	}
-	return (1);
-}
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * Return: Always 0 (Success)
+ * main - function
+ * @argc: Number
+ * @argv: Array
+ * Return: 0 when successful, 1 on error
  */
-
 int main(int argc, char *argv[])
-
 {
+	int i, j, add = 0;
 
-
-	int c;
-	int str_to_int;
-	int result = 0;
-
-	c = 1;
-	while (c < argc)
+	for (i = 1; i <  argc; i++)
 	{
-		if (convert_to(argv[c]))
-
-		{
-			str_to_int = _convert_to_int(argv[c]);
-			result += str_to_int;
-		}
-
-
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-
-		c++;
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+	if (!isdigit(argv[i][k]))
+	{
+		printf("Error\n");
+		return (1);
 	}
-
-	printf("%d\n", result); /*print sum*/
-
+	}
+		add += _convert_to_int(argv[m]);
+	}
+	printf("%d\n", add);
 	return (0);
 }
-
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 int _putchar(char c);
 int _convert_to_int(char *s);
+int convert_to(char *str);
 #endif
