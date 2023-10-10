f8dc5dbcd3f6c50d9e078e2a9dda5795aa475ca1
ayuure <bertinaayuure0147@gmail.com>
2023-10-10 16:20:35 +0000

    2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - function
+ * @d: struct
+ * @name: one
+ * @age: two
+ * @owner: three
+ *
+ * Return (d): success
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}
