0bf62fab06e227b35df6cd94cb04fa2f7d262df9
ayuure <bertinaayuure0147@gmail.com>
2023-10-10 15:36:41 +0000

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - body
+ * @name: one
+ * @age: two
+ * @owner: three
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
