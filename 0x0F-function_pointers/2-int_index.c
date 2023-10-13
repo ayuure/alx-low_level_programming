f245b7b20026eded78339f692ae44456393219f0
ayuure <bertinaayuure0147@gmail.com>
2023-10-13 23:19:21 +0000

    not done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - return index
+ * @array: array
+ * @size: size
+ * @cmp: pointer
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int j;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (j = 0; j < size; j++)
+	{
+		if (cmp(array[j]))
+			return (j);
+	}
+	return (-1);
+}
+
