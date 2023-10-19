62cae71685733e45985f862aa9564dd07f36d187
ayuure <bertinaayuure0147@gmail.com>
2023-10-19 23:00:38 +0000

    done

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,14 +7,14 @@
  */
 void free_list(list_t *head)
 {
- list_t *t;
+list_t *t;
 
- while (head)
- {
- t = head->next;
- free(head->str);
- free(head);
- head = t;
- }
+while (head)
+{
+t = head->next;
+free(head->str);
+free(head);
+head = t;
+}
 }
 
