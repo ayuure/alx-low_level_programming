70203117c54fea63b0e77726a2fca4dc5bd0f999
ayuure <bertinaayuure0147@gmail.com>
2023-10-27 23:03:35 +0000

    done done

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - function
+ *
+ * Return: 1 if little endian, 0 if big endian
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *e = (char *)&n;
+
+	if (*e)
+	return 1;
+	else
+	return 0;
+}
+
