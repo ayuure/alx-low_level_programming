6386fb91b0bf67120c1546259ca1a8f5cf4a84e9
ayuure <bertinaayuure0147@gmail.com>
2023-12-12 00:26:18 -0800

    added

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+ /**
+ * add - addition func
+ * @a: number1
+ * @b: number2
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub function
+ * @a: number1
+ * @b: number2
+ *
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul function
+ * @a: number1
+ * @b: number2
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div function
+ * @a: number1
+ * @b: number2
+ *
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modules function
+ * @a: number1
+ * @b: number2
+ *
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
