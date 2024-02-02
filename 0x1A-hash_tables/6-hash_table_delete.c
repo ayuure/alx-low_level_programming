621184a33f2efb49fdea7887409935847825d9a3
ayuure <bertinaayuure0147@gmail.com>
2024-02-02 13:25:47 +0000

    done

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -17,7 +17,7 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		while (ht->array[i] != NULL)
 		{
-			next = ht->array[i]->n;
+			n = ht->array[i]->next;
 			free(ht->array[i]->key);
 			free(ht->array[i]->value);
 			free(ht->array[i]);
